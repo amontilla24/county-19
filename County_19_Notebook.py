9f2c3449b6e580f3327d80481ccd826697c33efc
Alberto Montilla <amontilla2008@gmail.com>
2020-08-30 15:07:39 +0200

    Script convert

--- County_19_Notebook.py
+++ County_19_Notebook.py
@@ -14,7 +14,7 @@
 # #### iOS / Android App (Flutter?)
 # Frontend to access the Data
 
-# In[138]:
+# In[203]:
 
 
 # Imports
@@ -30,7 +30,7 @@ import os
 import logging
 
 
-# In[139]:
+# In[204]:
 
 
 # Filter data by a given county
@@ -41,7 +41,7 @@ def filter_by_county(county):
     return filtered
 
 
-# In[140]:
+# In[205]:
 
 
 # Filter data by a given state
@@ -50,7 +50,7 @@ def filter_by_state(state):
     return us_states[(us_states['state']==state)].drop({'state', 'fips'},  axis=1).reset_index(drop=True)
 
 
-# In[141]:
+# In[206]:
 
 
 # Filter US data
@@ -59,21 +59,20 @@ def filter_us():
     return us.reset_index(drop=True)
 
 
-# In[142]:
+# In[207]:
 
 
 # Upload information info (county and state list) to firebase
 #
 def upload_info():
-    print('Uploading info data...')
     start = time.time()
     firebase_db.put(url='/info/', data=str(states.tolist()).strip('[]').replace('\'','').replace(', ', ','), name='states')
     firebase_db.put(url='/info/', data=str(counties_info.tolist()).strip('[]').replace('\'','').replace(', ', ','), name='counties')
     end = time.time()
-    print('Finished uploading info data (' + str(format(end-start, '.3f')) + ' secs)')
+    logging.info('\tUploaded info data (' + str(format(end-start, '.3f')) + ' secs)')
 
 
-# In[143]:
+# In[208]:
 
 
 # Upload given data to specific dir
@@ -83,42 +82,39 @@ def firebase_upload(dir, data, name):
     firebase_db.put(url=dir, data=data.head(90).to_dict('records'), name=name)
 
 
-# In[144]:
+# In[209]:
 
 
 def upload_us():
-    print('Uploading US data...')
     start = time.time()
     firebase_upload(dir='/', data=filter_us(), name='us')
     end = time.time()
-    print('Finished uploading US data (' + str(format(end-start, '.3f')) + ' secs)')
+    logging.info('\tUploaded US data (' + str(format(end-start, '.3f')) + ' secs)')
 
 
-# In[145]:
+# In[210]:
 
 
 def upload_states():
-    print('Uploading state data...')
     start = time.time()
     for state in states:
         firebase_upload(dir='/state/', data=filter_by_state(state), name=state)
     end = time.time()
-    print('Finished uploading states data (' + str(format(end-start, '.3f')) + ' secs)')
+    logging.info('\tUploaded states data (' + str(format(end-start, '.3f')) + ' secs)')
 
 
-# In[146]:
+# In[211]:
 
 
 def upload_counties(n):
-    print('Uploading county data...')
     start = time.time()
     for county in counties[n%len(counties)]:
         firebase_upload(dir='/county/', data=filter_by_county(county), name=county.replace(".", ""))
     end = time.time()
-    print('Finished uploading county data (' + str(format(end-start, '.3f')) + ' secs)')
+    logging.info('\tUploaded county data (' + str(format(end-start, '.3f')) + ' secs)')
 
 
-# In[151]:
+# In[212]:
 
 
 def upload_all_data(n):
@@ -128,19 +124,18 @@ def upload_all_data(n):
     upload_states()
     upload_counties(n)
     end = time.time()
-    print('Total upload time: (' + str(format(end-start, '.3f')) + ' secs)')
+    logging.info('Total upload time: (' + str(format(end-start, '.3f')) + ' secs)')
 
 
-# In[152]:
+# In[213]:
 
 
 def partition(county_list, n):
-    """Yield successive n-sized chunks from lst."""
     for i in range(0, len(county_list), n):
         yield county_list[i:i + n]
 
 
-# In[153]:
+# In[214]:
 
 
 # Init log
@@ -153,31 +148,21 @@ repo_url = 'https://github.com/nytimes/covid-19-data.git'
 
 if not os.path.isdir('./covid-19-data'):
     git.Git(clone_dir).clone(repo_url)
-    logging.info(str('Pulled data from ' + repo_url))
+    logging.info(str('\tCloned repo: ' + repo_url))
         
-git_repo = git.cmd.Git(repo_dir)
-logging.info(str("Pulling data:"+git_repo.pull()))
-
-# Populating dataframes
-logging.info('Populating dataframes... ')
-start = time.time()
-us_counties = pd.read_csv('covid-19-data/us-counties.csv')
-us_states = pd.read_csv('covid-19-data/us-states.csv')
-us = pd.read_csv('covid-19-data/us.csv')
-us_counties.sort_values(by=['date'], inplace=True, ascending=False)
-us_states.sort_values(by=['date'], inplace=True, ascending=False)
-us.sort_values(by=['date'], inplace=True, ascending=False)
-end = time.time()
-logging.info(str('Success populating dataframes (' + str(format(end-start, '.3f')) + ' secs)'))
+global git_repo
+global us_counties
+global us_states
+global us
+global firebase_url
+global firebase_db
+global counties_info
+global counties
+global states
 
 # Connect to firebase database
 firebase_url = 'https://county-live-19.firebaseio.com/'
 firebase_db = firebase.FirebaseApplication(firebase_url, None)
-
-# Get information data
-counties_info = us_counties.county.unique()
-counties = list(partition(counties_info, 60))
-states = us_states.state.unique()
 
 
 # In[ ]:
@@ -186,12 +171,11 @@ states = us_states.state.unique()
 # Upload data to firebase
 i = 0
 while True:
-    
+    logging.info('\t========== ['+datetime.today().strftime('%m/%d/%Y %H:%M:%S')+'] =========')
     git_repo = git.cmd.Git(repo_dir)
-    logging.info(str("Pulling data:"+git_repo.pull()))
-
+    logging.info(str("\tPulled data: "+git_repo.pull()))
+    
     # Populate dataframes
-    logging.info('Populating dataframes... ')
     start = time.time()
     us_counties = pd.read_csv('covid-19-data/us-counties.csv')
     us_states = pd.read_csv('covid-19-data/us-states.csv')
@@ -200,15 +184,12 @@ while True:
     us_states.sort_values(by=['date'], inplace=True, ascending=False)
     us.sort_values(by=['date'], inplace=True, ascending=False)
     end = time.time()
-    logging.info(str('Success populating dataframes (' + str(format(end-start, '.3f')) + ' secs)'))
+    logging.info(str('\tPopulated dataframes (' + str(format(end-start, '.3f')) + ' secs)'))
     counties_info = us_counties.county.unique()
     counties = list(partition(counties_info, 60))
     states = us_states.state.unique()
 
     # Upload data
-    logging.info(str("Pulling data:"+git_repo.pull()))
-    print('Uploading Data ['+datetime.today().strftime('%m/%d/%Y %H:%M:%S')+']')
-    logging.info(str('Uploading Data ['+datetime.today().strftime('%m/%d/%Y %H:%M:%S')+']'))
     upload_all_data(i)
     i = (i + 1) % len(counties)
     time.sleep(1200)
